8d5a90d3fa26dfcecf34a50e41ca9865a5877816
SalimAlsaadi <al-saadis-s-s@hotmail.com>
2023-06-13 03:48:59 -0700

    explain to how use class with constructor

---
+++ Constructor.cs
@@ -0,0 +1,27 @@
+﻿
+namespace practiceForClassAndConstructors
+{
+     class Constructor
+    {
+       
+        public long id;
+        public string name;
+        public string grade;
+        public string registrationNumber;
+
+        public Constructor(long id, string name, string registrationNumber)//constructor. must be same name of class name.
+        {
+            this.id = id;
+            this.name = name;
+            this.registrationNumber = registrationNumber;
+            //stdDo();
+            Console.WriteLine("student name: " + name + " student id: " + id + " student registrationNumber: " + registrationNumber + " is pass with perfect grade");
+
+        }
+
+        public void stdDo()//method. we can use in any where
+        {
+            Console.WriteLine("student name: " + name + " student id: " + id + " student registrationNumber: " + registrationNumber + " is pass with perfect grade");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace practiceForClassAndConstructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Constructor std1 = new Constructor(123, "salim", "SA123");
+
+            Constructor std2 = new Constructor(252, "ali", "A252");
+        }
+    }
+}
